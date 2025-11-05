8fd3c108fb211f5702364af17273fd7456c04657
Wanjos-eng <weslen_anjos@outlook.com>
2025-11-04 22:49:53 -0300

    feat: add cadastro feature components

---
+++ app/cadastro/cliente/page.tsx
@@ -0,0 +1,58 @@
+import Link from 'next/link';
+
+export default function RegisterPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
+      <div className="w-full max-w-xs">
+        <form className="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label className="block text-gray-400 text-sm font-bold mb-2" htmlFor="name">
+              Nome
+            </label>
+            <input 
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="name"
+              type="text"
+              placeholder="Nome"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-400 text-sm font-bold mb-2" htmlFor="email">
+              Email
+            </label>
+            <input 
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="Email"
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-400 text-sm font-bold mb-2" htmlFor="password">
+              Senha
+            </label>
+            <input 
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="******************"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button 
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Criar Conta
+            </button>
+            <Link href="/login/cliente">
+              <p className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
+                Já tenho uma conta
+              </p>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
