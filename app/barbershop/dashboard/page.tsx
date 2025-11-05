b60d44ef5e6aae83fd715450d38e4a413aa4609e
Wanjos-eng <weslen_anjos@outlook.com>
2025-11-04 22:49:40 -0300

    feat: add barbershop feature components

---
+++ app/barbershop/dashboard/page.tsx
@@ -0,0 +1,8 @@
+// app/barbershop/dashboard/page.tsx
+export default function BarbershopDashboard() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl">Página da Barbearia</h1>
+    </div>
+  );
+}
