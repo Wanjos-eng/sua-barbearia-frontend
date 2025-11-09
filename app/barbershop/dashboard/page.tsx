4f44b5488264b6f0473006943c00f78dbbb87869
Arthur Guilherme <arthurguilhermesb@gmail.com>
2025-11-09 12:28:43 -0300

    fix: erro no SidebarItem. Adicionado return para correção

--- app/barbershop/dashboard/page.tsx
+++ app/barbershop/dashboard/page.tsx
@@ -47,7 +47,7 @@ interface ActiveBarber {
 // Componentes
 // Componente Item da Barra Lateral
 const SidebarItem: React.FC<SidebarItemProps> = ({ icon: Icon, label, active }) => {
-  <button
+ return (<button
     className={`
       flex items-center w-full px-4 py-3 text-sm font-medium rounded-lg
       transition-colors duration-150
@@ -60,7 +60,7 @@ const SidebarItem: React.FC<SidebarItemProps> = ({ icon: Icon, label, active }) 
   >
       <Icon className="w-5 h-5 mr-3"/>
       <span>{label}</span>
-  </button>
+  </button>)
 };
 
 //Componente Barra Lateral
