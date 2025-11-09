929bd84232f0bfe083f947600cd6d9ba52450387
Arthur Guilherme <arthurguilhermesb@gmail.com>
2025-11-09 11:46:31 -0300

    feat: adicionado componente item da barra lateral

--- app/barbershop/dashboard/page.tsx
+++ app/barbershop/dashboard/page.tsx
@@ -1,8 +1,65 @@
 // app/barbershop/dashboard/page.tsx
-export default function BarbershopDashboard() {
-  return (
-    <div className="p-8">
-      <h1 className="text-2xl">Página da Barbearia</h1>
-    </div>
-  );
+import React from 'react';
+import {
+  LayoutGrid,
+  Users,
+  Calendar,
+  DollarSign,
+  User,
+  TrendingUp,
+  Percent,
+  Clock,
+  Check,
+  RefreshCw
+} from 'lucide-react';
+
+// Tipos (Typescript)
+interface SidebarItemProps{
+  icon: React.ElementType;
+  label: string;
+  active?: boolean;
 }
+
+interface StatsCardProps{
+  icon: React.ElementType;
+  title: string;
+  value: string;
+  iconBgColor: string;
+}
+
+interface Appointment{
+  time: string,
+  client: string;
+  barber: string;
+  service: string;
+  price: string;
+  status: 'Pendente' | 'Confirmado';
+}
+
+interface ActiveBarber {
+  initials: string;
+  name: string;
+  total: number;
+  next: number;
+}
+
+// Componentes
+// Item da Barra Lateral
+const SidebarItem: React.FC<SidebarItemProps> = ({ icon: Icon, label, active }) => {
+  <button
+    className={`
+      flex items-center w-full px-4 py-3 text-sm font-medium rounded-lg
+      transition-colors duration-150
+      ${
+        active
+          ? 'bg-gray-800 text-white'
+          : 'hover:bg-gray-700 hover:text-white'
+      }
+    `}
+  >
+      <Icon className="w-5 h-5 mr-3"/>
+      <span>{label}</span>
+  </button>
+}
+
+//export default function Dashboard
