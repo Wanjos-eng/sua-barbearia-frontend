445f8b475691f2310220ac8826c1bea81fa0decb
Arthur Guilherme <arthurguilhermesb@gmail.com>
2025-11-13 09:29:51 -0300

    feat: Adicionado componente principal da aplicação, estados para controle de dados e hangler para atualização de estado do form

--- app/cadastro/barbearia/page.tsx
+++ app/cadastro/barbearia/page.tsx
@@ -15,3 +15,31 @@ interface FormData{
 
 // Definindo o tipo de usuário
 type UserType = 'barbearia' | 'barbeiro';
+
+// Componente principal da aplicação
+export default function RegisterBarbPage(){
+    // Estado para controlar o tipo de usuario
+    const [UserType, setUserType] = useState<UserType>('barbearia');
+
+    // Estado para controlar os dados do formulário
+    const [formData, setFormData] = useState<FormData>({
+        nome: '',
+        email: '',
+        telefone: '',
+        endereco: '',
+        senha: '',
+        confirmarSenha: '',
+    });
+
+    // Hangler para atualizar o estado do formulário
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setFormData((prev) => ({...prev, [name]: value,}));
+
+    }
+
+    // Estilos comuns para os inputs
+    const inputBaseStyle = "w-full bg-gray-800 border border-cyan-600 rounded-lg py-3 pl-10 pr-3 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:border-transparent transition-all duration-300";
+
+
+}
