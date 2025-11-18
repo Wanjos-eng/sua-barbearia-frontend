3db5f1fbf92cd35ff3c6fdfeb31936e0378ca4c5
brenonovais2 <breno.novais@discente.univasf.edu.br>
2025-11-18 13:21:58 -0300

    feat: adiciona ícones de agendamento e usuário

---
+++ app/dashboard/cliente/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+// app/barbershop/dashboard/page.tsx
+import React from 'react';
+import {
+    Scissors,
+    Calendar,
+    Clock,
+    User,
+    Star,
+    Phone,
+    Info,
+    X,
+    Check,
+    CheckCircle,
+    Loader2,
+    ChevronLeft,
+    Edit,
+    LogOut,
+    Mail,
+    Lock
+} from 'lucide-react';
+import { Stats } from 'fs';
