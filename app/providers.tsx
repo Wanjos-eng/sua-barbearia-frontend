4a5800ed349dbde5125659cdb85a959ec869c927
Wanjos-eng <weslen_anjos@outlook.com>
2025-11-21 09:50:46 -0300

    chore: add providers wrapper

---
+++ app/providers.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { AuthProvider } from '@/app/contexts/AuthContext';
+import { ReactNode } from 'react';
+
+export function Providers({ children }: { children: ReactNode }) {
+  return (
+    <AuthProvider>
+      {children}
+    </AuthProvider>
+  );
+}
