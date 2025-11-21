b6ad541b26415416631f8ec0d23f115850d42372
Weslen Anjos <51387737+Wanjos-eng@users.noreply.github.com>
2025-11-21 12:40:59 -0300

    Revert "Weslen config"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import { Inter } from 'next/font/google';
-import { Providers } from './providers'; // Importando o novo componente
 import './globals.css';
 
 // Configura a fonte Inter
-const inter = Inter({
+const inter = Inter({ 
   subsets: ['latin'],
   weight: ['700'], // Adiciona o peso 700 (Bold) que você especificou
   variable: '--font-inter', // Opcional, mas útil
@@ -18,10 +17,7 @@ export default function RootLayout({
     <html lang="pt-br">
       {/* Adiciona a classe da fonte ao body */}
       <body className={`${inter.className} bg-black text-white`}>
-        {/* Envolve os children com o Providers */}
-        <Providers>
-          {children}
-        </Providers>
+        {children}
       </body>
     </html>
   );
